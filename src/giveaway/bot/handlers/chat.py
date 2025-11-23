2ececfb904a0cfaf714953e0d0e0f1768dcc8102
Paylak Urusyan <urusy001@umn.edu>
2025-11-22 21:50:07 -0600

    Added ai anti-spam

--- src/giveaway/bot/handlers/chat.py
+++ src/giveaway/bot/handlers/chat.py
@@ -1,12 +1,10 @@
 from aiogram import Router
 from aiogram.types import Message
 
-from config import LOGS_DIR
 from src.antispam.test_classifier import is_spam
 from src.helpers import append_message_to_csv
 
 router = Router(name="chat")
-file_path = LOGS_DIR / "messages.csv"
 
 @router.message(lambda message: "peptide_rus" == getattr(message.chat, "username", ""))
 async def handle_chat_message(message: Message):
