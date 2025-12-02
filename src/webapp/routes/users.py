a4c4542797f9cda2f356fec294e0751fde3cce09
Paylak Urusyan <urusy001@umn.edu>
2025-12-01 20:15:22 -0600

    Merge remote-tracking branch 'origin/main'

--- src/webapp/routes/users.py
+++ src/webapp/routes/users.py
@@ -16,5 +16,6 @@ async def get_users(
         db: AsyncSession = Depends(get_db),
 ):
     user = await get_user(db, column_name, value)
+    print(user or 'no user')
     if not user: return []
     else: return [user]
