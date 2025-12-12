7b3a42e97c79d603e5d346dcc0969b058c727354
Paylak Urusyan <urusy001@umn.edu>
2025-12-12 01:30:41 -0600

    Payment process

--- src/webapp/static/js/app/pages/process-payment.js
+++ src/webapp/static/js/app/pages/process-payment.js
@@ -24,7 +24,7 @@ export async function renderProcessPaymentPage(order_number) {
     profilePageEl.style.display = "none";
 
     // Header / nav
-    headerTitle.textContent = "";
+    headerTitle.textContent = `Номер заказа ${order_number}`;
     searchBtnEl.style.display = "flex";
     navBottomEl.style.display = "flex";
 
