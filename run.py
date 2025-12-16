924a4356849c8f439b1f14fbc2f3b37dcffe9940
Paylak Urusyan <urusy001@umn.edu>
2025-12-16 17:22:57 -0600

    Adding TG Categories biach

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ import logging
 import asyncio
 import signal
 
-from src.ai.bot.main import run_new_bot, run_dose_bot, run_professor_bot
-from src.tg_methods import client as tg_client
+from src.admin_panel.bot.main import run_admin_bot
+from src.delivery.sdek import client as cdek_client
+from src.onec import OneCEnterprise
+from src.webapp.main import run_app
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,11 +18,11 @@ logger = logging.getLogger("main")
 
 
 async def main():
-    await tg_client.start()
     tasks = [
-        asyncio.create_task(run_new_bot()),
-        asyncio.create_task(run_dose_bot()),
-        asyncio.create_task(run_professor_bot()),
+        asyncio.create_task(OneCEnterprise().postgres_worker()),
+        asyncio.create_task(run_admin_bot()),
+        asyncio.create_task(run_app()),
+        asyncio.create_task(cdek_client.token_worker())
     ]
 
     async def shutdown():
