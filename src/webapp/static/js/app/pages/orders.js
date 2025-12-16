f56c8da314219a32ceaf03349a8493c9f3b7e346
Paylak Urusyan <urusy001@umn.edu>
2025-12-16 12:37:01 -0600

    Working on history

--- src/webapp/static/js/app/pages/orders.js
+++ src/webapp/static/js/app/pages/orders.js
@@ -8,48 +8,6 @@ import {
     toolbarEl,
     tosOverlayEl
 } from "./constants.js";
-import {state} from "../state.js";
-import {apiGet} from "../../services/api.js";
-
-export async function renderOrdersPage() {
-    navBottomEl.style.display = "flex";
-    headerTitle.textContent = "Магазин ElixirPeptide";
-    tosOverlayEl.style.display = "none";
-    listEl.style.display = "none";
-    toolbarEl.style.display = "none";
-    searchBtnEl.style.display = "none";
-    detailEl.style.display = "none";
-    cartPageEl.style.display = "none";
-    checkoutPageEl.style.display = "none";
-    contactPageEl.style.display = "none";
-    paymentPageEl.style.display = "none";
-    processPaymentEl.style.display = "none";
-    profilePageEl.style.display = "none";
-    ordersPageEl.style.display = "block";
-    orderDetailEl.style.display = "none";
-    await getUserCarts();
-
-}
-
-async function getUserCarts() {
-    const user = state.user
-    if (user) {
-        const user_id = user.tg_id;
-        const result = await apiGet(`/cart/?user_id=${user_id}`);
-        alert(JSON.stringify(result));
-    }
-}
-
-import {
-    cartPageEl, checkoutPageEl, contactPageEl,
-    detailEl,
-    headerTitle,
-    listEl,
-    navBottomEl, orderDetailEl, ordersPageEl, paymentPageEl, processPaymentEl, profilePageEl,
-    searchBtnEl,
-    toolbarEl,
-    tosOverlayEl
-} from "./constants.js";
 
 import { state } from "../state.js";
 import { apiGet } from "../../services/api.js";
