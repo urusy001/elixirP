e4806e5973c244fa6afcca0de38b6277c1993ebb
Paylak Urusyan <urusy001@umn.edu>
2025-12-16 21:38:22 -0600

    Added balance bih

--- src/amocrm/client.py
+++ src/amocrm/client.py
@@ -474,6 +474,7 @@ class AsyncAmoCRM:
             if not notes:
                 return None
 
+            [print(note.get("params") or {}).get("text") for note in notes]
             for note in notes:
                 text = (note.get("params") or {}).get("text")
                 if text and 'Cостав заказа' in str(text).strip():
