5db147b6da23b72ce9fa2426de5b9b7bb61b65ab
Paylak Urusyan <urusy001@umn.edu>
2025-12-17 19:00:53 -0600

    Fixing yandex

--- src/webapp/routes/webhooks.py
+++ src/webapp/routes/webhooks.py
@@ -5,19 +5,24 @@ router = APIRouter(prefix="/webhooks", tags=["Webhooks"])
 
 @router.get("/amocrm")
 async def get_webhook(request: Request):
-    print(await request.json())
+    try: print(await request.json())
+    except: print(await request.body())
+
 
 
 @router.post("/amocrm")
 async def get_webhook(request: Request):
-    print(await request.json())
+    try: print(await request.json())
+    except: print(await request.body())
 
 
 @router.put("/amocrm")
 async def get_webhook(request: Request):
-    print(await request.json())
+    try: print(await request.json())
+    except: print(await request.body())
 
 
 @router.delete("/amocrm")
 async def get_webhook(request: Request):
-    print(await request.json())
+    try: print(await request.json())
+    except: print(await request.body())
