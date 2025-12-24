6ea1235bb83924c5652dfddf0a519e48de22cab0
Paylak Urusyan <urusy001@umn.edu>
2025-12-23 23:38:51 -0600

    Cart status updates

--- src/ai/bot/handlers/admin.py
+++ src/ai/bot/handlers/admin.py
@@ -33,7 +33,7 @@ async def handle_pin(message: Message):
     forwarded_message = message.reply_to_message
     print(forwarded_message.html_text)
     c_id = forwarded_message.forward_from_chat.id
-    await message.answer(text=forwarded_message.html_text, reply_markup=admin_keyboards.open_test, parse_mode='HTML')
+    await message.bot.send_message(chat_id=-1003002316616, text=forwarded_message.html_text, reply_markup=admin_keyboards.open_test, parse_mode='HTML')
     await message.bot.send_message(text=forwarded_message.html_text, chat_id=c_id, reply_markup=admin_keyboards.open_test, parse_mode='HTML')
 
 @new_admin_router.message(Command('set_premium'))
