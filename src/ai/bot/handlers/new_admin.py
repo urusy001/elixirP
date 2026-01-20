9ecddd67a021ad772556a85c4719336f56d19b8f
Paylak Urusyan <urusy001@umn.edu>
2026-01-20 16:47:10 -0600

    Admin Panel

--- src/ai/bot/handlers/new_admin.py
+++ src/ai/bot/handlers/new_admin.py
@@ -189,7 +189,7 @@ async def handle_inline_query(inline_query: InlineQuery, state: FSMContext):
         value = data[2]
         if column_name == "id":
             if value.isdigit(): value = int(value)
-            else: result = InlineQueryResultArticle(id=uuid4(), title="Ошибка поиска", input_message_content=InputTextMessageContent(message_text="<b>Телеграм ID должен быть числом</b>"))
+            else: result = InlineQueryResultArticle(id=str(uuid4()), title="Ошибка поиска", input_message_content=InputTextMessageContent(message_text="<b>Телеграм ID должен быть числом</b>"))
 
         async with get_session() as session: rows, total = await search_users(session, column_name, value)
         [print(UserCreate.model_validate(row)) for row in rows]
